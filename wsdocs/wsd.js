1fa1a4b21b2d603825cfd297afc9b4504e96cc19
shinbaad <shinbaad@gmail.com>
2021-07-29 18:53:00 +0900

    add wsd.js

--- wsdocs/wsd.js
+++ wsdocs/wsd.js
@@ -4,7 +4,14 @@ const wsd = new WebSocket.Server({port:7811});
 
 wsd.on("connection", function(ws) {
   ws.on('message', function(message){
-    console.log('message ' + message);
+    var v = new Uint8Array(message);
+    var dbg = '';
+    for ( i=0 ; i<v.length ; i++ )
+    {
+      dbg += v[i] + ' ';
+    }
+    console.log(dbg);
+
   });
 });
   
